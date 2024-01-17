e4f01e44aa9f56ed606f6dc3031dc567c47a4e87
Witherking25 <witherking@withertech.com>
2024-01-17 08:42:48 -0600

    fixed casing issue with the hash library response type

--- src/ts/AchievementsManager.ts
+++ src/ts/AchievementsManager.ts
@@ -710,7 +710,7 @@ export class AchievementManager implements Manager
 		})
 		if (response.success)
 		{
-			this.hashes = (JSON.parse(response.result.body.toLowerCase()) as {MD5List: Record<string, number>}).MD5List
+			this.hashes = (JSON.parse(response.result.body.toLowerCase()) as {md5list: Record<string, number>}).md5list
 		}
 		await this.refresh();
 	}
