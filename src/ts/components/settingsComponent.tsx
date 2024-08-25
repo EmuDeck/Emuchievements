1e0b68a9d974ba7a35b8bf8c82149c70bdea1f3b
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2024-08-25 21:58:38 +0300

    Add CustomIdsOverrides component

--- src/ts/components/settingsComponent.tsx
+++ src/ts/components/settingsComponent.tsx
@@ -1,7 +1,9 @@
 import
 	{
 		FC,
+		useEffect,
 		useRef,
+		useState,
 		VFC
 	} from "react";
 import
@@ -12,7 +14,9 @@ import
 	SidebarNavigation,
 	TextField,
 	ToggleField,
-	Navigation
+	Navigation,
+	Dropdown,
+	ButtonItem,
 } from "decky-frontend-lib";
 import { useEmuchievementsState } from "../hooks/achievementsContext";
 import { ReactMarkdown, ReactMarkdownOptions } from "react-markdown/lib/react-markdown";
@@ -125,6 +129,146 @@ const RetroAchievementsSettings: VFC = () =>
 	</div>);
 };
 
+const CustomIdsOverrides: VFC = () => {
+	type TableRowsProps = {
+		appId: string | undefined | null;
+		retroAchievementAppId: string | undefined;
+	}
+
+	const [tableRows, setTableRows] = useState<TableRowsProps[]>([])
+	const emuchievementsState = useEmuchievementsState();
+	const onChangeRetroAchievementsId = (index: number, appId: string) => {
+		const newRows = [...tableRows]
+		newRows[index].retroAchievementAppId = appId;
+
+		setTableRows(newRows);
+	}
+
+	const onGameChange = (index: number, appId: string) => {
+		const newRows = [...tableRows];
+		newRows[index].appId = appId;
+
+		setTableRows(newRows);
+	}
+
+	const { custom_ids_overrides }= emuchievementsState.settings.data.cache;
+	const gameOptions = Object.keys(custom_ids_overrides)
+		.map((item) => {
+			const idAsNumber = Number.parseInt(item);
+			const currentApp = custom_ids_overrides[idAsNumber];
+
+			return {
+				data: item,
+				appId: item,
+				retroAchievementAppId: currentApp.retro_achivement_game_id,
+				label: currentApp.name ?? item,
+			}
+		}).sort((a, b) => a.label.localeCompare(b.label));
+
+	useEffect(() => {
+		setTableRows([
+			...gameOptions
+				.filter((option) => option.retroAchievementAppId)
+				.map((item) => ({
+					appId: `${item.appId}`,
+					retroAchievementAppId: `${item.retroAchievementAppId}`
+				})),
+			{
+				appId: undefined,
+				retroAchievementAppId: undefined,
+			},
+		])
+	}, [])
+
+	const save = async () => {
+		const appsToAdd = tableRows.filter((row) => row.appId);
+
+		for (const app of appsToAdd) {
+			const { appId, retroAchievementAppId } = app;
+
+			if (!appId || !retroAchievementAppId) {
+				continue
+			}
+
+			const appIdAsNumber = Number.parseInt(appId)
+
+			emuchievementsState.settings.data.cache.custom_ids_overrides[appIdAsNumber] = {
+				...emuchievementsState.settings.data.cache.custom_ids_overrides[appIdAsNumber],
+				retro_achivement_game_id: Number.parseInt(retroAchievementAppId)
+			};
+		}
+
+		await emuchievementsState.settings.writeSettings();
+	}
+
+	const addRow = () => {
+		setTableRows([...tableRows, {
+			appId: undefined,
+			retroAchievementAppId: undefined,
+		}])
+	}
+
+	const t = useTranslations();
+
+	return (
+		<div style={{
+			marginTop: '40px',
+			height: 'calc(100% - 40px)',
+		}}>
+			<ButtonItem layout="below" onClick={() => save()}>
+				{ t('settingsCustomIdsOverridesSave')}
+			</ButtonItem>
+
+			<PanelSection>
+				<div style={{overflow: 'scroll',height: '100%',}}>
+					<div
+						className="header-row"
+						style={{ gridTemplateColumns: '75% 25%', display: 'grid'}}
+					>
+						<div style={{padding: '10px 10px',textAlign: 'center',fontWeight: 600}}>
+							Game
+						</div>
+
+						<div style={{padding: '10px 10px',textAlign: 'center',fontWeight: 600}}>
+							RA Id
+						</div>
+					</div>
+				</div>
+
+				{tableRows.map((row, idx) => (
+					<Focusable
+						key={row.appId}
+						flow-children="horizontal"
+						style={{
+							gridTemplateColumns: '75% 25%',
+							padding: '10px 10px 10px 0px',textAlign: 'center',display: 'grid',
+						}}
+					>
+						<Dropdown
+							rgOptions={gameOptions}
+							selectedOption={row.appId}
+							onChange={(e) => onGameChange(idx, e.data)}
+						/>
+
+						<TextField
+							style={{ marginLeft: '1rem'}}
+							mustBeNumeric
+							defaultValue={row.retroAchievementAppId}
+							onChange={(e) =>
+								onChangeRetroAchievementsId(idx, e.target.value)
+							}
+						/>
+					</Focusable>
+				))}
+
+				<ButtonItem layout="below" onClick={() => addRow()}>
+					{ t('settingsCustomIdsOverridesAddRow')}
+				</ButtonItem>
+			</PanelSection>
+		</div>
+	);
+}
+
 export const SettingsComponent: VFC = () =>
 {
 	const t = useTranslations();
@@ -138,6 +282,9 @@ export const SettingsComponent: VFC = () =>
 			title: t("settingsRetroAchievements"),
 			content: <RetroAchievementsSettings />,
 			route: '/retroachievements'
+		},
+		{
+			title: t("settingsCustomIdsOverrides"),
+			content: <CustomIdsOverrides />,
 		}
 	]} />;
-};
